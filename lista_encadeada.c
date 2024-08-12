a724c138c73f5aa6d0ca86f83bf29cc8899ad773
VicenteZanatta <130722342+VicenteZanatta@users.noreply.github.com>
2024-08-12 19:14:06 +0000

    Update lista_encadeada.c

--- lista_encadeada.c
+++ lista_encadeada.c
@@ -1,141 +1,210 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct avaliacao{
+typedef struct avaliacoes {
     float notas;
-    struct avaliacao *prox;
-}Avaliacao;
+    struct avaliacoes *prox;
+} Avaliacoes;
 
-typedef struct lista_de_avaliacoes{
+/*typedef struct lista_de_avaliacoes {
     Avaliacao *cabeca;
-}Lista_notas;
+} Lista_notas;*/
 
-typedef struct aluno{
-    long int matricula;    
-    char nome[30]; 
+typedef struct aluno {
+    int matricula;
+    char nome[30];
     char curso[20];
-    int  ingresso;
+    int ingresso;
     int frequencia[18];
-    Lista_notas *avaliacoes; //lista de tamanho variavel, aumenta com o cadastro de avaliações 
+    int aula;
+    Avaliacoes *avaliacao; // lista de tamanho variável, aumenta com o cadastro de avaliações 
     struct aluno *prox;
-}Aluno;
+} Aluno;
 
-typedef struct lista{
+typedef struct lista {
     Aluno *cabeca;
-    Aluno *prox;
-}Lista_alunos;
+} Lista_alunos;
 
-Aluno *acessa(Lista_alunos *L, int i){
+Aluno *acessa(Lista_alunos *L, int i) {
     Aluno *aluno = L->cabeca;
     int j = 0;
-    while(aluno != NULL && j != i){
-        aluno = aluno -> prox;
+    while (aluno != NULL && j != i) {
+        aluno = aluno->prox;
         j++;
     }
     return aluno;
-}//acessa a matricula do aluno referente a matricula armazenada na lista encadeada
+}// acessa a matrícula do aluno referente a matrícula armazenada na lista encadeada
 
-Aluno *busca(Lista_alunos *L, int x){
-    Aluno *aluno = L -> cabeca;
-    int j = 0;
-    while(aluno != NULL && aluno -> matricula != x){
-        aluno = aluno -> prox;
+void excluir_aluno(Lista_alunos *L, int matricula) {
+    Aluno *atual = L->cabeca;
+    Aluno *anterior = NULL;
+
+    if (atual == NULL) {
+        printf("Lista de alunos vazia. Nada a excluir.\n");
+        return;
+    }
+
+    while (atual != NULL && atual->matricula != matricula) {
+        anterior = atual;
+        atual = atual->prox;
+    }
+
+    if (atual == NULL) {
+        printf("Aluno com matrícula %d não encontrado.\n", matricula);
+        return;
+    }
+
+    if (anterior == NULL) {
+        L->cabeca = atual->prox;
+    } else {
+        anterior->prox = atual->prox;
+    }
+
+    free(atual);
+    printf("Aluno com matrícula %d excluído com sucesso.\n", matricula);
+}
+
+Aluno *busca(Lista_alunos *L, int x) {
+    Aluno *aluno = L->cabeca;
+    while (aluno != NULL && aluno->matricula != x) {
+        aluno = aluno->prox;
     }
     return aluno;
 }
 
-void inserir_aluno(Aluno *novo_aluno, Lista_alunos *lista)
-{
-    Aluno *aux = lista->cabeca;
-    
-    if(aux == NULL)
-    {
-        lista->prox = novo_aluno;
+void inserir_aluno(Aluno *novo_aluno, Lista_alunos *lista) {
+    if (lista->cabeca == NULL) {
+        lista->cabeca = novo_aluno;
         novo_aluno->prox = NULL;
-    }
-    else
-    { 
-        while(aux->prox != NULL)
+    } else {
+        Aluno *aux = lista->cabeca;
+        while (aux->prox != NULL)
             aux = aux->prox;
-    
+
         aux->prox = novo_aluno;
         novo_aluno->prox = NULL;
     }
-}//insere um no Aluno na lista de alunos
+}// insere um no Aluno na lista de alunos
 
-void cadastrar_aluno(Lista_alunos *lista) 
-{
-    char str[30];
-    Aluno *no = malloc(sizeof(Aluno));
+void cadastrar_avaliacao(Aluno **tabela) {
 
-    if (no == NULL) 
+    float nota = 0;
+    int matricula = 0;
+    int id = 0;
+    int cond = 1;
+    Aluno *no;
+    Avaliacoes *avl;
+    Avaliacoes *aux;
+
+    printf("___________________________");
+    printf("\nCADASTRO DE AVALIACOES (PARA VOLTAR DIGITE 0): ");
+
+    while(1)
     {
-        printf("ERRO AO ALOCAR MEMORIA");
-        return;
-    }
+        cond = 1;
+        printf("\nMatricula: ");
+        scanf("%i", &matricula);
 
-    printf("\n___________________________\n");
-    printf("\nCADASTRO DE ALUNO (PARA VOLTAR DIGITE 0)\n");
+        if(matricula == 0)
+            break;
+        else
+        {
+            id = funcaoespalhamento(matricula);
+
+            if(tabela[id] == NULL)
+            {
+                printf("\nMATRICULA NAO ENCONTRADA\n");
+                cond = 0;
+            }
+            else
+            {
+                no = tabela[id];
+            }
+
+            while(cond)
+            {
+                printf("\nNOTA: ");
+                scanf("%2f", &nota);
+                cond = (nota == -1) ? 0 : 1;
+
+                printf("\na\n");
+                if(nota >= 0)
+                {
+                    avl = malloc(sizeof(Avaliacoes));
+                    
+                    avl->notas = nota;
+                    avl->prox = NULL;
+                    printf("\nb\n");
+                    if(no->avaliacao == NULL)
+                    {
+                      no->avaliacao = avl;  
+                    }
+                    else
+                    {
+                        aux = no->avaliacao;
+                        printf("\nd");
+                        while(aux->prox != NULL)
+                            printf("\nc\n");
+                            aux = aux->prox;
+                        
+                        aux = avl;
+                        printf("\ne");
+                    }
+                    printf("\nf");
+                }
+                printf("\ng");
+
+            }
+            printf("\nh");
+        }
+        printf("\ni");
+
+    }
+}
+
+void realizar_chamada(Lista_alunos *lista) {
     
-    printf("\nMATRICULA: ");
-    scanf("%ld", &no->matricula);
+    Aluno *no = lista->cabeca;
 
-    if (no->matricula == 0)
-    {
-        free(no);
-        return;
-    }
-
-    getchar(); 
-
-    printf("NOME: ");
-    fgets(no->nome, sizeof(no->nome), stdin);
-    no->nome[strcspn(no->nome, "\n")] = '\0'; 
-
-    printf("CURSO: ");
-    fgets(str, sizeof(str), stdin);
-    str[strcspn(str, "\n")] = '\0'; 
-    strcpy(no->curso, str);
-
-    printf("ANO DE INGRESSO: ");
-    scanf("%i", &no->ingresso);
-
-    Lista_notas *notas = malloc(sizeof(Lista_notas));
-    if (notas == NULL) 
-    {
-        free(no);
-        printf("ERRO AO ALOCAR MEMORIA PARA AS AVALIACOES");
-        return;
-    }
-
-    notas->cabeca = NULL;
-    no->avaliacoes = notas;
-    
-    inserir_aluno(no, lista);
-
-    printf("\nCADASTRO REALIZADO COM SUCESSO\n");
-    printf("\nMATRICULA: %ld\n", no->matricula);
-    printf("NOME: %s\n", no->nome);
-    printf("CURSO: %s\n", no->curso);
-    printf("INGRESSO: %i\n", no->ingresso); 
-    printf("\n___________________________\n");
-
-}//solicita os dados para o cadastro de um aluno e, se ja existem avaliacoes no sistema, pedeas notas contabilizadas.  Se ja existem chamadas realizadas no sistema, solicita tambem a presenca do aluno em cada um dos dias
-
-void exibir_lista(Lista_alunos *lista)
-{
-    Aluno* no = lista->cabeca;
+    printf("\nREALIZANDO CHAMDA\n");
 
     while(no != NULL)
     {
-        printf("\n%ld", no->matricula);
-        printf("\n%s", no->nome);
-        printf("\n%s", no->curso);
-        printf("\n%d\n",no->ingresso);
+        printf("%s | %i: ", no->nome, no->matricula);
+        scanf("%i", &no->frequencia[no->aula]);
+        no->aula++;
         no = no->prox;
+
     }
 
-    printf("*******************");
+   /* int i = 0;
+
+    for(;i <= 18; i++)
+    {
+        printf("%i ", no->frequencia[i]);
+    }
+    printf("\n");*/
+
+}// contabiliza a frequência dos alunos em um determinado dia, perguntando quem está presente e ausente. Assim que um aluno atingir 10 faltas, deve imprimir um aviso dizendo que o mesmo foi reprovado por infrequência.
+
+void exibir_lista(Lista_alunos *lista) {
+    Aluno *no = lista->cabeca;
+
+    if(lista->cabeca == NULL)
+    {
+        printf("\nNAO HA ALUNOS CADASTRADOS\n");
+    }
+    else
+    { 
+        while (no != NULL) {
+            printf("\nMatrícula: %d", no->matricula);
+            printf("\nNome: %s", no->nome);
+            printf("\nCurso: %s", no->curso);
+            printf("\nIngresso: %d\n", no->ingresso);
+            no = no->prox;
+        }
+    }
+
+    printf("___________________________");
 }
