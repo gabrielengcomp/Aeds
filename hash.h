3687d3dab07b5af0593ec9ff989c7fa1e2b6f8dc
Gabriel Santos <164521051+gabrielengcomp@users.noreply.github.com>
2024-09-09 07:09:53 -0300

    Delete hash.h

--- hash.h
+++
@@ -1,9 +0,0 @@
-#ifdef __HASH__
-#define __HASH__
-
-int funcaoespalhamento(int chave);
-void inserir_hash(Aluno *tabela[], int matricula, Aluno *endereco);
-Aluno* buscarHash (Aluno *tabela[], int matricula); //busca na tabela e retorna ponteiro para aluno 
-void exibir_tabela_hash(Aluno *tabela[]);
-
-#endif
